55cc67b91079b809d8a3509e31a210bc0848846a
Tim Babb <trbabb@gmail.com>
2023-08-24 00:36:13 -0700

    trivial test passes

--- test/test-graph.cpp
+++ test/test-graph.cpp
@@ -1,8 +1,15 @@
+#define TEST_MODULE_NAME test_graph
+
 #include <iostream>
+#include <pcg_random.hpp>
+#include <gtest/gtest.h>
 
 #include <graph/graph.h>
 
-int main() {
-    
-    return 0;
+using namespace graph;
+
+TEST(TEST_MODULE_NAME, test_create) {
+    DirectedGraph<int, float> g;
+    EXPECT_EQ(g.vertices_size(), 0);
+    EXPECT_EQ(g.edges_size(), 0);
 }
